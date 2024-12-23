9c517ea2f85fc924964dab44964fb559d8210019
saiful10400 <saifulislam10400i5@gmail.com>
2024-12-24 00:33:16 +0600

    dist folder added.

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import app from "./app";
 function main(){
     try{
         app.listen(5000,()=>{
-            console.log("this server is running at 5000 port.")
+            console.log("Server is running at 5000 port.")
         })
     }catch(err){
         console.log(err)
